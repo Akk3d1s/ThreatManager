a4f0b0f2566d33a575e3480f43a61dc75c52cea9
Justin Lo <justinlctstudy96@gmail.com>
2021-10-16 04:20:27 +0800

    threat_list.py: db.session.query user join replaced filter for innerjoin, add file attachment count inside threat innerjoin result

--- app/controllers/threat_list.py
+++ app/controllers/threat_list.py
@@ -9,20 +9,19 @@ from app.models.threat_status import ThreatStatus
 from app.models.threat_category import ThreatCategory
 from app.models.comment import Comment
 from app.models.application_role import RoleApplication
+from app.models.file import ThreatFile
 from werkzeug.urls import url_parse
-from sqlalchemy import and_, or_, not_
+from sqlalchemy import and_, or_, not_, func
 
 @app.route('/threat')
 @login_required
 def threat():
     # citizen role
     if current_user.role_id == 1:
-        # threats = current_user.threats
-        # threats = db.session.query(User, Threat, ThreatStatus, ThreatCategory).filter(User.id==Threat.user_id).filter(Threat.status_id==ThreatStatus.id).filter(Threat.category_id==ThreatCategory.id).filter(User.id==current_user.id).all()
         form = ThreatCommentForm()
         userRoles = db.session.query(UserRole).all()
-        threats = db.session.query(User, UserRole, Threat, ThreatStatus, ThreatCategory).filter(User.role_id==UserRole.id).filter(User.id==Threat.user_id).filter(Threat.status_id==ThreatStatus.id).filter(Threat.category_id==ThreatCategory.id).filter(User.id==current_user.id).all()
-        comments = db.session.query(User, UserRole, Comment).filter(User.role_id==UserRole.id).filter(User.id==Comment.user_id).all()
+        threats = db.session.query(Threat, ThreatStatus, ThreatCategory, User, UserRole, func.count(ThreatFile.id).filter(ThreatFile.threat_id==Threat.id).label("file_count")).join(ThreatStatus).join(ThreatCategory).join(User).join(UserRole).group_by(Threat).all()
+        comments = db.session.query(User, Comment, UserRole).join(Comment).join(UserRole).all()
         return render_template("citizen.html", title='Home Page', threats=threats, comments=comments, userRoles=userRoles, form=form)
     # police roles
     elif current_user.role_id == 2:
@@ -31,14 +30,14 @@ def threat():
     elif current_user.role_id == 3:
         form = ThreatCommentForm()
         userRoles = db.session.query(UserRole).all()
-        threats = db.session.query(User, UserRole, Threat, ThreatStatus, ThreatCategory).filter(User.role_id==UserRole.id).filter(User.id==Threat.user_id).filter(Threat.status_id==ThreatStatus.id).filter(Threat.category_id==ThreatCategory.id).all()
-        comments = db.session.query(User, UserRole, Comment).filter(User.role_id==UserRole.id).filter(User.id==Comment.user_id).all()
+        threats = db.session.query(Threat, ThreatStatus, ThreatCategory, User, UserRole).join(ThreatStatus).join(ThreatCategory).join(User).join(UserRole).all()
+        comments = db.session.query(User, Comment, UserRole).join(Comment).join(UserRole).all()
         return render_template("editor.html", title='Home Page', userRoles=userRoles, threats=threats, comments=comments, form=form)
     elif current_user.role_id == 4:
         form = ThreatCommentForm()
         userRoles = db.session.query(UserRole).all()
-        threats = db.session.query(User, UserRole, Threat, ThreatStatus, ThreatCategory).filter(User.role_id==UserRole.id).filter(User.id==Threat.user_id).filter(Threat.status_id==ThreatStatus.id).filter(Threat.category_id==ThreatCategory.id).filter(or_(Threat.status_id==2, Threat.status_id==4)).all()
-        comments = db.session.query(User, UserRole, Comment).filter(User.role_id==UserRole.id).filter(User.id==Comment.user_id).all()
+        threats = db.session.query(Threat, ThreatStatus, ThreatCategory, User, UserRole).join(ThreatStatus).join(ThreatCategory).join(User).join(UserRole).all()
+        comments = db.session.query(User, Comment, UserRole).join(Comment).join(UserRole).all()
         threatCategories = db.session.query(ThreatCategory).all()
         return render_template("approver.html", title="Home Page", userRoles=userRoles, threats=threats, comments=comments, threatCategories=threatCategories, form=form)
     else:
