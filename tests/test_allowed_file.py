84776d64679115db6d887573cb994304a2bb419a
Justin Lo <justinlctstudy96@gmail.com>
2021-10-21 20:32:16 +0800

    deleted sys in test_allowed_file.py

--- tests/test_allowed_file.py
+++ tests/test_allowed_file.py
@@ -1,8 +1,4 @@
-
-import sys
-sys.path.append('..')
-from app import app
 from app.controllers.report import allowed_file
 
 def test_allowed_file():
-    assert allowed_file('xsxx.jpg') == True
+    assert allowed_file('xsxx.jpeg') == True
