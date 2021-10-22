edc84fc469354a4c62c1c6ef4bb8550fbfa4ab47
Henry Van Rooyen <akkedis123@gmail.com>
2021-10-22 07:22:57 +0200

    reach > 80% coverage for login

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,7 @@
 import pytest
-from app import app
 import pyotp
+from app import app
+from app.models.user import User
 
 
 @pytest.fixture
@@ -11,7 +12,20 @@ def client():
         yield client
 
 
-def test_login(client, monkeypatch):
+def test_already_logged_in(client, monkeypatch):
+    '''Test already logged in'''
+
+    with client.session_transaction() as sess:
+        sess['_fresh'] = True
+        sess['_id'] = '123'
+        sess['_user_id'] = '1'
+        sess['login_attempts'] = 0
+
+    rv = login(client, None, None, None)
+    assert 'Logout' in rv.get_data(as_text=True)
+
+
+def test_successful_login(client, monkeypatch):
     '''Test successful login'''
     email = 'admin@police.com'
     password = 'admin'
@@ -23,7 +37,37 @@ def test_login(client, monkeypatch):
     monkeypatch.setattr(pyotp.TOTP, 'verify', mockreturn)
 
     rv = login(client, email, password, totp)
-    assert b'Role Applications' in rv.data
+    assert 'Logout' in rv.get_data(as_text=True)
+
+
+def test_failed_login_invalid_user(client):
+    '''Test failed login, user doesnt exist'''
+    email = 'test@mail.com'
+    password = 'test'
+    totp = '123'
+    
+    rv = login(client, email, password, totp)
+    assert 'Invalid email, password or TOTP code.' in rv.get_data(as_text=True)
+
+def test_failed_login_user_inactive(client, monkeypatch):
+    '''Test failed login, user not yet activated'''
+    email = 'admin@police.com'
+    password = 'admin'
+    totp = '123'
+
+    user = User.query.filter_by(email=email).first()
+    user.is_active = False
+    user.save()
+
+    # TOPT should return True
+    def mockreturn(param1, param2):
+        return True
+    monkeypatch.setattr(pyotp.TOTP, 'verify', mockreturn)
+
+    rv = login(client, email, password, totp)
+    assert 'Please confirm account.' in rv.get_data(as_text=True)
+    user.is_active = True
+    user.save()
 
 
 def login(client, email, password, totp):
